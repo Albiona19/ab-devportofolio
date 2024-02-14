b1be8ef57e1d3a16c9341c94af89f76a899143c3
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2024-02-14 16:19:56 +0200

    some style added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,25 @@ import {fadeIn, textVariant} from '../utils/motion'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>
+        Introduction
+      </p>
+      <h2 className={styles.sectionHeadText}>
+        Overview.
+
+      </h2>
+    </motion.div>
+    <motion.p
+    variants={fadeIn("","",0.1,1)}>
+        I'm a skilled front end developer with experience in Html, Css and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js, AngularJs and more. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life! 
+    </motion.p>
+    </>
   )
 }
 
