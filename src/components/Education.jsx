1afe25bc66af0cc6ed531fb0a062c2856ad57377
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2024-02-14 20:16:35 +0200

    education section

---
+++ src/components/Education.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { education } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+
+
+const Education = () => {
+  return (
+   <>
+   <motion.div variants={textVariant()}>
+   <p className={styles.sectionSubText}>what I have studied so far</p>
+      <h2 className={styles.sectionHeadText}>Education.</h2>
+
+   </motion.div>
+
+    <div className="mt-20 flex flex-col">
+      <VerticalTimeline>
+        {education.map((education))}
+      </VerticalTimeline>
+
+
+    </div>
+   
+   
+   
+   
+   
+   
+   </>
+  )
+}
+
+export default SectionWrapper(Education,"education")
--- src/constants/index.js
+++ src/constants/index.js
@@ -114,13 +114,13 @@ import {
     },
   ];
   
-  const experiences = [
+  const education = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
+      title: "Computer Science and Engineering",
+      company_name: "UBT",
       icon: starbucks,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "October 2019 - October 2023",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -262,4 +262,4 @@ import {
     },
   ];
   
-  export { services, technologies, experiences, testimonials, projects };
+  export { services, technologies, education, testimonials, projects };
