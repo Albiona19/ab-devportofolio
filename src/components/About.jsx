5f103b17d701e9b52fadd974a5efd515e516448b
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2024-02-14 19:22:18 +0200

    services added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,12 @@ import {styles} from '../styles'
 import {services} from '../constants'
 import {fadeIn, textVariant} from '../utils/motion'
 
+const ServiceCard=({index,title,icon})=>{
+  return(
+    <p>{services.title}</p>
+  )
+}
+
 const About = () => {
   return (
     <>
@@ -18,13 +24,20 @@ const About = () => {
       </h2>
     </motion.div>
     <motion.p
-    variants={fadeIn("","",0.1,1)}>
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
         I'm a skilled front end developer with experience in Html, Css and
         JavaScript, and expertise in frameworks like React, Node.js, and
         Three.js, AngularJs and more. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life! 
     </motion.p>
+    <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   )
 }
